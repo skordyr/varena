b3901a074b328e9c1dcbe885a53c1a7c08b6a25f
yxie <yxie@leyantech.com>
2026-05-21 15:14:41 +0800

    refactor(design-system): :art: alphabetize exports in index.ts

--- src/design-system/index.ts
+++ src/design-system/index.ts
@@ -1,3 +1,4 @@
+export * from "./animation";
 export * from "./border";
 export * from "./color";
 export * from "./effect";
@@ -5,7 +6,6 @@ export * from "./filter";
 export * from "./interactivity";
 export * from "./layout";
 export * from "./spacing";
-export * from "./animation";
 export * from "./typography";
 
 export * from "./design";
